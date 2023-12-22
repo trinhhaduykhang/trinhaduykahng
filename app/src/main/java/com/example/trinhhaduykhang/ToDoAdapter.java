70ce9de9d3006cd4d8b05276424c19e3dbc28c15
khang123 <khang113trinh@gmail.com>
2023-12-22 17:40:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/trinhhaduykhang/ToDoAdapter.java
@@ -0,0 +1,42 @@
+package com.example.trinhhaduykhang;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ToDoAdapter extends ArrayAdapter<ToDo> {
+    private Context context;
+    private List<ToDo> tasks;
+
+    public ToDoAdapter(Context context, List<ToDo> tasks) {
+        super(context, 0, tasks);
+        this.context = context;
+        this.tasks = tasks;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ToDo task = getItem(position);
+        if (convertView == null) {
+//            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo, parent, false);
+        }
+
+        TextView taskTitle = convertView.findViewById(R.id.task_title);
+        TextView taskContent = convertView.findViewById(R.id.task_content);
+        TextView taskDate = convertView.findViewById(R.id.task_date);
+//        TextView taskType = convertView.findViewById(R.id.task_type);
+
+        taskTitle.setText(task.getTitle());
+        taskContent.setText(task.getContent());
+        taskDate.setText(task.getDate());
+//        taskType.setText(task.getType());
+
+        return convertView;
+    }
+}
